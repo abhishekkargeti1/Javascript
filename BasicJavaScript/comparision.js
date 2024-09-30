17f4ff3ae7f1378ca7fac6bcbfc7a553d0e67f27
AbhishekKargeti1 <abhishek.karegti1@gmail.com>
2024-09-30 20:42:24 +0530

    Added New Concepts

---
+++ BasicJavaScript/comparision.js
@@ -0,0 +1,25 @@
+// console.log(2>3);
+// console.log(2==2);
+// console.log(2<=4);
+// console.log(4>=2);
+// console.log(4!=0);
+// console.log(5===3);
+
+
+// console.log("2" >1)
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined >0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+
+// strict check
+
+console.log("2" === 2);
+
+console.log(true === 1);
+
+
--- BasicJavaScript/datatypes.js
+++ BasicJavaScript/datatypes.js
@@ -2,14 +2,14 @@
 
 // alert("Hello")  we are using node js not browser
 
-console.log(3+3);
-console.log("Abhishek")
+// console.log(3+3);
+// console.log("Abhishek")
 
 // Datatype
 
-let name="Abhishek"
-let age = 18
-let isloggedIn = true
+// let name="Abhishek"
+// let age = 18
+// let isloggedIn = true
 
 // number
 // string
@@ -22,6 +22,80 @@ let isloggedIn = true
 
 // object 
 
-console.log(typeof null); // object 
-console.log(typeof undefined); // undefined
+// console.log(typeof null); // object 
+// console.log(typeof undefined); // undefined
+
+
+// Primitive DataType
+
+// Call By Value 
+
+// 1 String
+// 2 number
+// 3 Boolean 
+// 4 Null => empty not 0 
+// 5 undefined
+// 6 symbol
+// 7 BigInt
+
+
+// Reference Type or Non Primitve Type
+
+// 1 Arrays
+// 2 Objects
+// 3 Function
+
+
+// What Type of lanaguage is Javascript ?
+// Answer is  dynamically  tyoed Language  
+
+const number = 32
+console.log(typeof number);
+const salary  = 32.54
+console.log(typeof salary);
+const isloggedIn = false    
+console.log(typeof isloggedIn);
+const outsidetemp = null
+console.log(typeof outsidetemp);
+
+
+// symbol
+
+const id = Symbol('123')
+const id1 = Symbol('123')
+
+console.log(id === id1);
+
+// bigint
+const number2 = 455165851851541515185545n
+console.log(typeof number2);
+
+
+// Non Primitive Datatypes
+
+// Array
+const hero = ["Abhishek", "Nikhil", "Kargeti"]
+
+// Object 
+let myobj={
+    name :"Hero",
+    age :21
+}
+ 
+
+// Function 
+
+let fun=function (){
+    return "Hello I am Function";
+}
+
+console.log(fun());
+
+console.log(typeof fun);
+
+
+
+
+
+
 
